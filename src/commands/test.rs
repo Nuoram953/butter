267d9f79f1a9e5a6fce192dbd28771a369243719
Nuoram953 <antoinemaroun@gmail.com>
2026-06-16 12:30:58 -0400

    feat: add basic `test` command

---
+++ src/commands/test.rs
@@ -0,0 +1,5 @@
+use anyhow::{Ok, Result};
+
+pub fn handle() -> Result<()> {
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use clap::{Parser, Subcommand};
+
+mod commands;
+
+#[derive(Subcommand)]
+enum Commands {
+    Test {},
 }
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Test {} => commands::test::handle(),
+    }?;
+
+    Ok(())
+}
+
+//create files in .config/butter
+//create commands
+//parse rules.yml
