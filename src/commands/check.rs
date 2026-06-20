7511462e6b18bac677ead66dd281924083201e5c
Nuoram953 <antoinemaroun@gmail.com>
2026-06-20 09:40:04 -0400

    feat: output to console using new module

--- src/commands/check.rs
+++ src/commands/check.rs
@@ -1,10 +1,9 @@
 use anyhow::{Ok, Result};
-use colored::Colorize;
 use log::debug;
 
 use crate::{
-    config::{self, RuleConfig},
-    rules::{Level, Rule},
+    config::{self},
+    output::printer::Printer,
 };
 
 pub fn handle() -> Result<()> {
@@ -12,40 +11,13 @@ pub fn handle() -> Result<()> {
 
     for rule in config.rules {
         debug!("Checking for {:#?}", rule);
-        let passed = match rule {
-            RuleConfig::File(ref r) => r.evaluate(),
-        };
 
-        print_result(&rule, passed);
+        let passed = rule.evaluate();
+
+        let printer = Printer::new();
+
+        printer.print_result(&rule, passed);
     }
 
     Ok(())
-}
-
-fn print_result(rule: &RuleConfig, passed: bool) {
-    const WIDTH: usize = 80;
-
-    let (name, message, level) = match rule {
-        RuleConfig::File(r) => (&r.name, &r.message, &r.level),
-    };
-
-    let result_text = match passed {
-        true => "success".green(),
-        false => match level {
-            Level::Warn => "warning".yellow(),
-            Level::Error => "failure".red(),
-        },
-    };
-
-    let dots = WIDTH.saturating_sub(
-        name.len() + result_text.len() + 2, // spaces around dots
-    );
-
-    let dots = ".".repeat(dots);
-
-    println!("{} {} {}", name, dots, result_text);
-
-    if !passed {
-        println!("  {}", message.yellow());
-    }
 }
